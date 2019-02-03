c812c7e7bfe18330213c65d58e113d42eb82b8ec
SYVYUA <47115106+SYVYUA@users.noreply.github.com>
2019-02-03 11:46:04 +0200

    edit package

--- app/src/main/java/com/scare_book/MainActivity.java
+++ app/src/main/java/com/scare_book/MainActivity.java
@@ -4,6 +4,9 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 import com.hitomi.cmlibrary.CircleMenu;
 import com.hitomi.cmlibrary.OnMenuSelectedListener;
@@ -11,11 +14,23 @@ import com.scare_book.duration.ListDuration;
 import com.scare_book.for_gift.Gift;
 
 public class MainActivity extends AppCompatActivity {
-
+    Intent intent = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageButton giftBut = findViewById(R.id.giftbut);
+
+        giftBut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                intent = new Intent(MainActivity.this, Gift.class);
+                startActivity(intent);
+            }
+        });
+
+
         CircleMenu circleMenu = findViewById(R.id.cirlemenu);
         circleMenu.setMainMenu(Color.parseColor("#e69900"),R.drawable.add,R.drawable.cancel)
                 .addSubMenu(Color.parseColor("#990033"),R.drawable.chrome)
@@ -29,9 +44,6 @@ public class MainActivity extends AppCompatActivity {
                         Intent intent = null;
                         if(i == 0){
                         intent = new Intent(MainActivity.this, ListDuration.class);
-                        startActivity(intent);
-                        } else if (i == 1){
-                        intent = new Intent(MainActivity.this, Gift.class);
                         startActivity(intent);
                         }
                     }
