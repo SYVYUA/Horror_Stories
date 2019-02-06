3e1ecf9884520cde841cd07e3e94965b90d8b9a3
SYVYUA <47115106+SYVYUA@users.noreply.github.com>
2019-02-06 22:31:13 +0200

    edit

--- app/src/main/java/com/scare_book/history/HistoryDur01.java
+++ app/src/main/java/com/scare_book/history/HistoryDur01.java
@@ -6,12 +6,11 @@ package com.scare_book.history;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.method.ScrollingMovementMethod;
+
 import android.view.View;
 
-import android.widget.Button;
+import android.view.ViewGroup;
 import android.widget.LinearLayout;
-import android.widget.ScrollView;
 import android.widget.TextView;
 
 import com.scare_book.R;
@@ -19,40 +18,112 @@ import com.scare_book.R;
 public class HistoryDur01 extends AppCompatActivity implements View.OnClickListener {
 
     public static int a;
-    TextView nameHist, viewHist;
-    LinearLayout scrollHist;
-    private ScrollView mScrollContainer;
+    TextView nameHist1,nameHist2,nameHist3,nameHist4,nameHist5,nameHist6,nameHist7,nameHist8,nameHist9,nameHist10,nameHist11,nameHist12;
+    Intent intent = null;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_history_dur01);
 
-        scrollHist = findViewById(R.id.scroll_hist);
-        nameHist = findViewById(R.id.name_hist);
-        nameHist.setOnClickListener(this);
-
-        viewHist = findViewById(R.id.view_hist);
-
-
-        mScrollContainer = findViewById(R.id.scroll_container);
-
-
-
+        /*nameHist1 = findViewById(R.id.name_hist1);
+        nameHist1.setOnClickListener(this);
+        nameHist2 = findViewById(R.id.name_hist2);
+        nameHist2.setOnClickListener(this);
+        nameHist3 = findViewById(R.id.name_hist3);
+        nameHist3.setOnClickListener(this);
+        nameHist4 = findViewById(R.id.name_hist4);
+        nameHist4.setOnClickListener(this);
+        nameHist5 = findViewById(R.id.name_hist5);
+        nameHist5.setOnClickListener(this);
+        nameHist6 = findViewById(R.id.name_hist6);
+        nameHist6.setOnClickListener(this);
+        nameHist7 = findViewById(R.id.name_hist7);
+        nameHist7.setOnClickListener(this);
+        nameHist8 = findViewById(R.id.name_hist8);
+        nameHist8.setOnClickListener(this);
+        nameHist9 = findViewById(R.id.name_hist9);
+        nameHist9.setOnClickListener(this);
+        nameHist10 = findViewById(R.id.name_hist10);
+        nameHist10.setOnClickListener(this);
+        nameHist11 = findViewById(R.id.name_hist11);
+        nameHist11.setOnClickListener(this);
+        nameHist12 = findViewById(R.id.name_hist12);
+        nameHist12.setOnClickListener(this);*/
+        LinearLayout group = findViewById(R.id.but_container);
+        View v;
+        for(int i = 0; i < group.getChildCount(); i++) {
+            v = group.getChildAt(i);
+            if(v instanceof TextView) v.setOnClickListener(this);
+        }
     }
 
 
     @Override
     public void onClick(View v) {
         switch (v.getId()){
-            case R.id.name_hist:
+            case R.id.name_hist1:
                 a = 1;
-                Intent intent = new Intent(HistoryDur01.this, HistoryShow.class);
-
+                intent = new Intent(HistoryDur01.this, HistoryShow.class);
                 startActivity(intent);
-
-
             break;
+            case R.id.name_hist2:
+                a = 2;
+                intent = new Intent(HistoryDur01.this, HistoryShow.class);
+                startActivity(intent);
+                break;
+            case R.id.name_hist3:
+                a = 3;
+               intent = new Intent(HistoryDur01.this, HistoryShow.class);
+                startActivity(intent);
+                break;
+            case R.id.name_hist4:
+                a = 4;
+                intent = new Intent(HistoryDur01.this, HistoryShow.class);
+                startActivity(intent);
+                break;
+            case R.id.name_hist5:
+                a = 5;
+                intent = new Intent(HistoryDur01.this, HistoryShow.class);
+                startActivity(intent);
+                break;
+            case R.id.name_hist6:
+                a = 6;
+                intent = new Intent(HistoryDur01.this, HistoryShow.class);
+                startActivity(intent);
+                break;
+            case R.id.name_hist7:
+                a = 7;
+                intent = new Intent(HistoryDur01.this, HistoryShow.class);
+                startActivity(intent);
+                break;
+            case R.id.name_hist8:
+                a = 8;
+                intent = new Intent(HistoryDur01.this, HistoryShow.class);
+                startActivity(intent);
+                break;
+            case R.id.name_hist9:
+                a = 9;
+                intent = new Intent(HistoryDur01.this, HistoryShow.class);
+                startActivity(intent);
+                break;
+            case R.id.name_hist10:
+                a = 10;
+               intent = new Intent(HistoryDur01.this, HistoryShow.class);
+                startActivity(intent);
+                break;
+            case R.id.name_hist11:
+                a = 11;
+                intent = new Intent(HistoryDur01.this, HistoryShow.class);
+                startActivity(intent);
+                break;
+            case R.id.name_hist12:
+                a = 12;
+                intent = new Intent(HistoryDur01.this, HistoryShow.class);
+                startActivity(intent);
+                break;
+
         }
     }
 }
